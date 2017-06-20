ac728aa9f3d0b562ee31602219a6007de0d54d71
Navjot Singh <weavebytes@gmail.com>
2017-06-20 19:24:13 +0530

    tidy up tests

--- python/receiver1.py
+++ python/receiver1.py
@@ -2,10 +2,10 @@ import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 9876
 
-sock = socket.socket(socket.AF_INET, # Internet
-                      socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
  
 while True:
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+    # buffer size is 1024 bytes
+    data, addr = sock.recvfrom(1024) 
     print "received message:", data
--- python/sender1.py
+++ python/sender1.py
@@ -7,6 +7,5 @@ print "UDP target IP:", UDP_IP
 print "UDP target port:", UDP_PORT
 print "message:", MESSAGE
 
-sock = socket.socket(socket.AF_INET, # Internet
-                     socket.SOCK_DGRAM) # UDP
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
